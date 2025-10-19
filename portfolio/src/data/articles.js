22d7716b8a3036d2c455dffd323a3d2261acd1fb
annyone <annyone.vbg@gmail.com>
2025-10-19 21:47:53 +0300

    без картинок

--- portfolio/src/data/articles.js
+++ portfolio/src/data/articles.js
@@ -5,15 +5,13 @@ export const articles = [
     links: [
     { url: "https://dsgners.ru/raspberry/12236-rabota-nad-oshibkami", text: "dsgners" },
     { url: "https://habr.com/ru/articles/936856/", text: "habr" }
-    ],
-    thumbnail: '/images/articles/errors.jpg',
+    ]
   },
 {
     name: "Как я создала плагин для Figma с помощью ChatGPT",
     description: "Рассказала про свой опыт создания плагина для заметок с помощью нейросетей, который выглядит нативно и имеет настройку категорий",
     links: [
     { url: "https://habr.com/ru/articles/901112/", text: "habr" }
-    ],
-    thumbnail: '/images/articles/plugin.jpg',
+    ]
   }
 ];
