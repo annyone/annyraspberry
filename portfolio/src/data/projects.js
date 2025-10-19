e3af9b2d8362ea8881ebf4973d628c6960c19d1e
annyone <annyone.vbg@gmail.com>
2025-10-19 19:09:36 +0300

    картинки

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -4,7 +4,7 @@ const projects = [
     title: 'Редизайн основного раздела',
     excerpt: 'Переосмыслила и пересобрала интерфейс основного раздела продукта после MVP в условиях ограниченной вводной информации. Получила позитивную обратную связь об изменениях от пользователей после релиза.',
     content: 'Detailed description and notes about project one. Replace with your real content.',
-  thumbnail: '/data/logiq/thumbnail.png',
+  thumbnail: '/images/logiq/thumbnail.png',
     // `page` is the name of a component under src/casepages that will render this project's page.
     // If the module doesn't exist, the app will fall back to the generic template.
     page: 'logiq',
@@ -14,7 +14,7 @@ const projects = [
     title: 'Система управления турнирами',
     excerpt: 'С нуля спроектировала сложную систему для проведения турниров по дартс. Участвовала во всем цикле разработки от сбора требований до дизайн-ревью готовых фичей.',
     content: 'Detailed description and notes about project two. Replace with your real content.',
-  thumbnail: '/data/darts/thumbnail.png',
+  thumbnail: '/images/darts/thumbnail.png',
     page: 'darts'
   }
 ]
